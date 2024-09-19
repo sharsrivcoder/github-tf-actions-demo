66d75b401d7dda83d37eb575576c4df3fea66dee
Abhishek Pandey <51503468+Devabhi1@users.noreply.github.com>
2024-09-19 21:59:22 +0530

    update

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "s306022024"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = var.name
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum -y update
+              sudo yum -y install httpd
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
